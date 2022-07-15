4ff824e95a9597bdc53503e7a9d2cd3b615bd397
Deepshikha Ram <52650070+Deepshikha-Ram@users.noreply.github.com>
2022-07-15 12:43:47 +0530

    Create DigitalClock.cpp

---
+++ DigitalClock.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<windows.h>
+#include<ctime>
+
+using namespace std;
+
+int main(){
+    time_t t=time(NULL);
+    tm *timePtr=localtime(&t);
+
+    int seconds=(timePtr->tm_sec);
+    int minutes=(timePtr-> tm_min);
+    int hrs=(timePtr-> tm_hour);
+
+    while(true){
+        system("cls");
+        cout<<"The Digital time is : ";
+        cout<<" "<<hrs<<" : "<<minutes<<" : "<<seconds<<endl;
+        seconds++;
+
+        if(seconds>=60){
+            seconds=1;
+            minutes++;
+        }
+
+        if(minutes>=60){
+            minutes=0;
+            hrs++;
+        }
+
+        if(hrs>24){
+            hrs = 00;
+        }
+        Sleep(1000);
+    }
+    return 0;
+}
